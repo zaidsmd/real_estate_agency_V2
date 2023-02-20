48c7099e7451ba94dc08d0e89aa41b5adc5b03c4
zaidsmd <zaidsmd111@gmail.com>
2023-02-20 10:52:17 +0100

    set

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,1 +1,13 @@
 <?php
+$servername = "localhost";
+$user = "root";
+$password = '';
+$dbname = "real_estate";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $user, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
--- php/navbar.php
+++ php/navbar.php
@@ -1,1 +1,41 @@
+<nav>
+    <div id="logo">
+        <i class="fa-solid fa-house-chimney"></i>
+        <span>House Miner</span>
+    </div>
 <?php
+include_once "php/dbconfig.php";
+   if (isset($_COOKIE["user"])){
+       $id = $_COOKIE["user"]["id"];
+       $statement = $conn->prepare("SELECT * FROM `users` WHERE id = '$id'");
+       $statement->execute();
+       $result = $statement->fetchAll();
+       if (count($result)>0){
+           if ($_COOKIE["user"]["email"] == $result[0]["email"] && $_COOKIE["user"]["pwd"] == $result[0]["password"]){
+           ?>
+           <div class="navbar">
+               <ul class="nav-list">
+                   <li class="nav-item">
+                       <a href="index.php" class=" active nav-link">Home</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="" class="nav-link">Listings</a>
+                   </li>
+               </ul>
+               <button class="add-announcement"></button>
+           </div>
+           <a href="php/profile.php">
+               <div class="profile">
+                   <img src="<?= $result[0]["profile_pic"]?>" alt="">
+               </div>
+           </a>
+       <?php }else {
+           echo"<a id='login-home' href='php/login.php'>Se connecter</a>";
+       }
+   } else {
+       echo "<a id='login-home' href='php/login.php'>Se connecter</a>";
+   }}else {
+       echo "<a id='login-home' href='php/login.php'>Se connecter</a>";
+   }
+   ?>
+</nav>
